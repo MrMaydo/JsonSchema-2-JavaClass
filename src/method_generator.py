9d89e51fd3bd35588f6f6248c252c6074757506b
MrMaydo <89995856+MrMaydo@users.noreply.github.com>
2026-01-02 23:04:54 +0100

    refactor generate_hash_code

--- src/method_generator.py
+++ src/method_generator.py
@@ -25,6 +25,7 @@ JAVA_LITERALS = {
 indent_lvl1 = "    "
 indent_lvl2 = indent_lvl1 * 2
 indent_lvl3 = indent_lvl1 * 3
+return_indent = "        "
 
 
 @dataclass
@@ -75,7 +76,7 @@ def generate_getter(field: Field) -> str:
 
     _validate_java_identifier(field_name)
 
-    getter_name = "get" + field_name[0].upper() + field_name[1:]
+    getter_name = _get_getter_name(field)
 
     getter = [
         "",
@@ -94,7 +95,7 @@ def generate_setter(field: Field) -> str:
 
     _validate_java_identifier(field_name)
 
-    setter_name = "set" + field_name[0].upper() + field_name[1:]
+    setter_name = _get_setter_name(field_name)
 
     setter = [
         "",
@@ -123,12 +124,12 @@ def generate_equals(class_name: str, fields: List[Field]) -> str:
 
     for i, field in enumerate(fields):
         _validate_java_identifier(field.name)
-        getter_name = "get" + field.name[0].upper() + field.name[1:]
+        getter_name = _get_getter_name(field)
         semicolon = ";" if i == (len(fields) - 1) else ""
         if i == 0:
             equals.append(f"{indent_lvl2}return Objects.equals({getter_name}(), that.{getter_name}()){semicolon}")
         else:
-            equals.append(f"{indent_lvl2}        && Objects.equals({getter_name}(), that.{getter_name}()){semicolon}")
+            equals.append(f"{indent_lvl2}{return_indent}&& Objects.equals({getter_name}(), that.{getter_name}()){semicolon}")
     equals.append(f"{indent_lvl1}}}")
 
     return "\n".join(equals)
@@ -143,23 +144,37 @@ def generate_hash_code(fields: List[Field]) -> str:
 
     for i, field in enumerate(fields):
         _validate_java_identifier(field.name)
-        getter_name = "get" + field.name[0].upper() + field.name[1:]
-        comma = "," if i < (len(fields) - 1) else ""
-
-        if len(fields) == 1:
-            hash_code.append(f"{indent_lvl2}return Objects.hash({getter_name}());")
-        elif len(fields) > 1 and i == 0:
-            hash_code.append(f"{indent_lvl2}return Objects.hash(")
-            hash_code.append(f"{indent_lvl2}        {getter_name}(){comma}")
-        elif len(fields) > 1 and i == (len(fields) - 1):
-            hash_code.append(f"{indent_lvl2}        {getter_name}(){comma}")
-            hash_code.append(f"{indent_lvl2});")
-        else:
-            hash_code.append(f"{indent_lvl2}        {getter_name}(){comma}")
+        hash_code.extend(_get_return_hash_lines(fields, index=i))
 
     hash_code.append(f"{indent_lvl1}}}")
 
     return "\n".join(hash_code)
+
+
+def _get_return_hash_lines(fields, index) -> List[str]:
+    field = fields[index]
+    getter_name = _get_getter_name(field)
+    comma = "," if index < (len(fields) - 1) else ""
+    another_hash_line = f"{indent_lvl2}{return_indent}{getter_name}(){comma}"
+    if len(fields) == 1:
+        return [f"{indent_lvl2}return Objects.hash({getter_name}());"]
+    if len(fields) > 1 and index == 0:
+        return [f"{indent_lvl2}return Objects.hash(",
+                f"{another_hash_line}"]
+    if len(fields) > 1 and index == (len(fields) - 1):
+        return [f"{another_hash_line}",
+                f"{indent_lvl2});"]
+    return [another_hash_line]
+
+
+def _get_getter_name(field):
+    getter_name = "get" + field.name[0].upper() + field.name[1:]
+    return getter_name
+
+
+def _get_setter_name(field_name):
+    setter_name = "set" + field_name[0].upper() + field_name[1:]
+    return setter_name
 
 
 def _validate_java_identifier(name: str) -> None:
