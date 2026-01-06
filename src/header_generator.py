2236baf3df2097a7519a20862fab55d2babeb1fc
MrMaydo <89995856+MrMaydo@users.noreply.github.com>
2026-01-06 21:18:50 +0100

    fix set_package function

--- src/header_generator.py
+++ src/header_generator.py
@@ -2,7 +2,7 @@ import re
 
 
 def set_package(package: str) -> str:
-    PACKAGE_REGEX = r"^(?:[a-z_][a-z0-9_]*)(?:\.(?:[a-z_][a-z0-9_]*))*$"
+    PACKAGE_REGEX = r"^(?:[A-Za-z_][A-Za-z0-9_]*)(?:\.(?:[A-Za-z_][A-Za-z0-9_]*))*$"
     if not re.match(PACKAGE_REGEX, package):
         raise ValueError(f"Invalid package: '{package}'")
-    return f"package {package}"
+    return f"package {package};"
