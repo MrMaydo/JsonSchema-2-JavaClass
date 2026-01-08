4ef712925d5aba34621e91250de5e92730c76b4c
MrMaydo <89995856+MrMaydo@users.noreply.github.com>
2026-01-08 12:17:31 +0100

    Refactor java_method_generator.py

--- src/java_method_generator.py
+++ src/java_method_generator.py
@@ -14,21 +14,25 @@ def generate_fields_block(fields: List[Field]) -> str:
 
 
 def generate_field_declaration(field: Field) -> str:
-    _validate_java_identifier(field.name)
-    if field.description:
-        javadoc = "\n".join([
-            "",
-            f"{indent_lvl1}/**",
-            f"{indent_lvl1} * {field.description}",
-            f"{indent_lvl1} */"
-        ])
-    else:
-        javadoc = ""
+    _validate_java_field_name(field.name)
+
     declaration = [
-        javadoc,
+        _render_javadoc(field),
         f"{indent_lvl1}private {field.type} {field.name};"
     ]
     return "\n".join(declaration)
+
+
+def _render_javadoc(field: Field) -> str:
+    if field.description is None:
+        return ""
+
+    return "\n".join([
+        "",
+        f"{indent_lvl1}/**",
+        f"{indent_lvl1} * {field.description}",
+        f"{indent_lvl1} */"
+    ])
 
 
 def generate_getters_and_setters(fields: List[Field]) -> str:
@@ -41,40 +45,27 @@ def generate_getters_and_setters(fields: List[Field]) -> str:
 
 
 def generate_getter(field: Field) -> str:
-    field_name = field.name
-    field_type = field.type
-
-    _validate_java_identifier(field_name)
-
-    getter_name = _get_getter_name(field)
+    getter_name = _build_getter_name(field.name)
 
     getter = [
         "",
-        f"{indent_lvl1}public {field_type} {getter_name}() {{",
-        f"{indent_lvl2}return {field_name};",
+        f"{indent_lvl1}public {field.type} {getter_name}() {{",
+        f"{indent_lvl2}return {field.name};",
         f"{indent_lvl1}}}"
     ]
-    getter = "\n".join(getter)
-
-    return getter
+    return "\n".join(getter)
 
 
 def generate_setter(field: Field) -> str:
-    field_name = field.name
-    field_type = field.type
-
-    _validate_java_identifier(field_name)
-
-    setter_name = _get_setter_name(field_name)
+    setter_name = _build_setter_name(field.name)
 
     setter = [
         "",
-        f"{indent_lvl1}public void {setter_name}({field_type} {field_name}) {{",
-        f"{indent_lvl2}this.{field_name} = {field_name};",
+        f"{indent_lvl1}public void {setter_name}({field.type} {field.name}) {{",
+        f"{indent_lvl2}this.{field.name} = {field.name};",
         f"{indent_lvl1}}}"
     ]
-    setter = "\n".join(setter)
-    return setter
+    return "\n".join(setter)
 
 
 def generate_equals(class_name: str, fields: List[Field]) -> str:
@@ -89,66 +80,76 @@ def generate_equals(class_name: str, fields: List[Field]) -> str:
         f"{indent_lvl2}if (!(obj instanceof {class_name}))",
         f"{indent_lvl3}return false;",
 
-        f"{indent_lvl2}{class_name} that = ({class_name}) obj;"
+        f"{indent_lvl2}{class_name} that = ({class_name}) obj;",
+
+        _render_equals_return_statement(fields),
+        f"{indent_lvl1}}}"
     ]
 
-    for i, field in enumerate(fields):
-        _validate_java_identifier(field.name)
-        getter_name = _get_getter_name(field)
-        semicolon = ";" if i == (len(fields) - 1) else ""
-        if i == 0:
-            equals.append(f"{indent_lvl2}return Objects.equals({getter_name}(), that.{getter_name}()){semicolon}")
-        else:
-            equals.append(
-                f"{indent_lvl2}{return_indent}&& Objects.equals({getter_name}(), that.{getter_name}()){semicolon}")
-    equals.append(f"{indent_lvl1}}}")
-
     return "\n".join(equals)
+
+
+def _render_equals_return_statement(fields: List[Field]) -> str:
+    return_statement = []
+    for i, field in enumerate(fields):
+        getter_name = _build_getter_name(field.name)
+        end_line = ";" if i == (len(fields) - 1) else ""
+        if i == 0:
+            return_statement.append(f"{indent_lvl2}return Objects.equals({getter_name}(), that.{getter_name}()){end_line}")
+        else:
+            return_statement.append(
+                f"{indent_lvl2}{return_indent}&& Objects.equals({getter_name}(), that.{getter_name}()){end_line}")
+    return "\n".join(return_statement)
 
 
 def generate_hash_code(fields: List[Field]) -> str:
     hash_code = [
         "",
         f"{indent_lvl1}@Override",
-        f"{indent_lvl1}public int hashCode() {{"
+        f"{indent_lvl1}public int hashCode() {{",
+        _render_hashcode_return_statement(fields),
+        f"{indent_lvl1}}}"
     ]
-
-    for i, field in enumerate(fields):
-        _validate_java_identifier(field.name)
-        hash_code.extend(_get_return_hash_lines(fields, index=i))
-
-    hash_code.append(f"{indent_lvl1}}}")
 
     return "\n".join(hash_code)
 
 
-def _get_return_hash_lines(fields, index) -> List[str]:
-    field = fields[index]
-    getter_name = _get_getter_name(field)
-    comma = "," if index < (len(fields) - 1) else ""
-    another_hash_line = f"{indent_lvl2}{return_indent}{getter_name}(){comma}"
+def _render_hashcode_return_statement(fields: List[Field]) -> str:
     if len(fields) == 1:
-        return [f"{indent_lvl2}return Objects.hash({getter_name}());"]
-    if len(fields) > 1 and index == 0:
-        return [f"{indent_lvl2}return Objects.hash(",
-                f"{another_hash_line}"]
-    if len(fields) > 1 and index == (len(fields) - 1):
-        return [f"{another_hash_line}",
-                f"{indent_lvl2});"]
-    return [another_hash_line]
+        return _render_hashcode_return_statement_single_field(fields)
+
+    return _render_hashcode_return_statement_multiple_field(fields)
 
 
-def _get_getter_name(field):
-    getter_name = "get" + field.name[0].upper() + field.name[1:]
-    return getter_name
+def _render_hashcode_return_statement_single_field(fields: List[Field]) -> str:
+    field_name = fields[0].name
+    getter_name = _build_getter_name(field_name)
+    return f"{indent_lvl2}return Objects.hash({getter_name}());"
 
 
-def _get_setter_name(field_name):
-    setter_name = "set" + field_name[0].upper() + field_name[1:]
-    return setter_name
+def _render_hashcode_return_statement_multiple_field(fields: List[Field]) -> str:
+    return_statement = [f"{indent_lvl2}return Objects.hash("]
+    for index, field in enumerate(fields):
+        getter_name = _build_getter_name(field.name)
+        comma = "," if index < (len(fields) - 1) else ""
+        return_statement.append(f"{indent_lvl2}{return_indent}{getter_name}(){comma}")
+
+    return_statement.append(f"{indent_lvl2});")
+
+    return "\n".join(return_statement)
 
 
-def _validate_java_identifier(name: str) -> None:
+def _build_getter_name(field_name: str) -> str:
+    _validate_java_field_name(field_name)
+    return "get" + field_name[0].upper() + field_name[1:]
+
+
+def _build_setter_name(field_name: str) -> str:
+    _validate_java_field_name(field_name)
+    return "set" + field_name[0].upper() + field_name[1:]
+
+
+def _validate_java_field_name(name: str) -> None:
     if not name:
         raise ValueError("Field name cannot be empty")
 
