0c962a8adca4bb50275b38937253c5987a744f65
MrMaydo <89995856+MrMaydo@users.noreply.github.com>
2026-01-09 21:23:54 +0100

    refactor enum_generator.py

--- src/enum_generator.py
+++ src/enum_generator.py
@@ -21,40 +21,27 @@ def generate_enum_class(enum_class: EnumClass, package: str) -> str:
         "",
         f"import java.util.HashMap;",
         f"import java.util.Map;",
+        f"",
+        _get_javadoc(enum_class.description),
+        f"public enum {enum_class.name} {{",
+        _get_constants(enum_class.values),
+        f"",
+        f"{indent_lvl1}private final static Map<String, {enum_class.name}> CONSTANTS = new HashMap<String, {enum_class.name}>();",
+        _get_static_method(enum_class.name),
+        f"",
+        f"{indent_lvl1}private final String value;",
+        _get_constructor(enum_class.name),
+        _get_from_value_method(enum_class.name),
+        _get_to_string_method(),
+        _get_value_method(),
+        "}",
         f""
     ]
-
-    enum_body.extend(_get_javadoc(enum_class.description))
-
-    enum_body.append(f"public enum {enum_class.name} {{")
-    enum_body.extend(_get_constants(enum_class.values))
-
-    enum_body.append(f"")
-    enum_body.append(
-        f"{indent_lvl1}private final static Map<String, {enum_class.name}> CONSTANTS = new HashMap<String, {enum_class.name}>();")
-    enum_body.append(f"")
-    enum_body.extend(_get_static_method(enum_class.name))
-
-    enum_body.append(f"")
-    enum_body.append(f"{indent_lvl1}private final String value;")
-    enum_body.append(f"")
-    enum_body.extend(_get_constructor(enum_class.name))
-
-    enum_body.append(f"")
-    enum_body.extend(_get_from_value_method(enum_class.name))
-
-    enum_body.append(f"")
-    enum_body.extend(_get_to_string_method())
-
-    enum_body.append(f"")
-    enum_body.extend(_get_value_method())
-    enum_body.append("}")
-    enum_body.append(f"")
 
     return "\n".join(enum_body)
 
 
-def _get_javadoc(description: str) -> List[str]:
+def _get_javadoc(description: str) -> str:
     javadoc = [""]
     if description is not None:
         javadoc = [
@@ -63,41 +50,44 @@ def _get_javadoc(description: str) -> List[str]:
             f" * {description}",
             " */"
         ]
-    return javadoc
+    return "\n".join(javadoc)
 
 
-def _get_constants(constants: List[str]) -> List[str]:
+def _get_constants(constants: List[str]) -> str:
     values = []
     for i, value in enumerate(constants):
         line_end = ";" if i == (len(constants) - 1) else ","
         values.append(
             f'{indent_lvl1}{to_java_constant(value)}("{value}"){line_end}'
         )
-    return values
+    return "\n".join(values)
 
 
-def _get_static_method(class_name: str) -> List[str]:
+def _get_static_method(class_name: str) -> str:
     body = [
+        "",
         f"{indent_lvl1}static {{",
         f"{indent_lvl2}for ({class_name} c : values()) {{",
         f"{indent_lvl3}CONSTANTS.put(c.value, c);",
         f"{indent_lvl2}}}",
         f"{indent_lvl1}}}"
     ]
-    return body
+    return "\n".join(body)
 
 
-def _get_constructor(class_name: str) -> List[str]:
+def _get_constructor(class_name: str) -> str:
     body = [
+        "",
         f"{indent_lvl1}{class_name}(String value) {{",
         f"{indent_lvl2}this.value = value;",
         f"{indent_lvl1}}}"
     ]
-    return body
+    return "\n".join(body)
 
 
-def _get_from_value_method(class_name: str) -> List[str]:
+def _get_from_value_method(class_name: str) -> str:
     body = [
+        "",
         f"{indent_lvl1}public static {class_name} fromValue(String value) {{",
         f"{indent_lvl2}{class_name} constant = CONSTANTS.get(value);",
         f"{indent_lvl2}if (constant == null) {{",
@@ -107,23 +97,25 @@ def _get_from_value_method(class_name: str) -> List[str]:
         f"{indent_lvl2}}}",
         f"{indent_lvl1}}}"
     ]
-    return body
+    return "\n".join(body)
 
 
-def _get_to_string_method() -> List[str]:
+def _get_to_string_method() -> str:
     body = [
+        "",
         f"{indent_lvl1}@Override",
         f"{indent_lvl1}public String toString() {{",
         f"{indent_lvl2}return this.value;",
         f"{indent_lvl1}}}"
     ]
-    return body
+    return "\n".join(body)
 
 
-def _get_value_method() -> List[str]:
+def _get_value_method() -> str:
     body = [
+        "",
         f"{indent_lvl1}public String value() {{",
         f"{indent_lvl2}return this.value;",
         f"{indent_lvl1}}}"
     ]
-    return body
+    return "\n".join(body)
