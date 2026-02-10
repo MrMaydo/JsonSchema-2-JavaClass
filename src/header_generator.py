38e4fed56c707f53a11a16bf934b17d202a7a6a5
MrMaydo <89995856+MrMaydo@users.noreply.github.com>
2026-02-10 15:42:54 +0100

    add set imports to header generation

--- src/header_generator.py
+++ src/header_generator.py
@@ -1,4 +1,7 @@
 import re
+from typing import List
+
+from java_model import Field
 
 
 def set_package(package: str) -> str:
@@ -6,3 +9,19 @@ def set_package(package: str) -> str:
     if not re.match(PACKAGE_REGEX, package):
         raise ValueError(f"Invalid package: '{package}'")
     return f"package {package};"
+
+
+def set_imports(fields: List[Field]) -> str:
+    imports = [_imports["Objects"]]
+    for field in fields:
+        if field.type in _imports.keys():
+            imports.append(_imports[field.type])
+    imports.sort()
+    return "\n".join(imports)
+
+
+_imports = {
+    "Date": "import java.util.Date;",
+    "List": "import java.util.List;",
+    "Objects": "import java.util.Objects;",
+}
